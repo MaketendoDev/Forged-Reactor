36c0df23ed6444fc4026c3a62b9b0c4f68d4c86b
MaketendoDev <najim.oria59@hotmail.com>
2024-09-22 18:01:44 +0100

    More Hud

--- src/main/java/net/maketendo/forgedreactor/client/screens/Mk2ArmorHUDOverlay.java
+++ src/main/java/net/maketendo/forgedreactor/client/screens/Mk2ArmorHUDOverlay.java
@@ -31,7 +31,7 @@ import com.mojang.blaze3d.platform.GlStateManager;
 
 @Mod.EventBusSubscriber({Dist.CLIENT})
 public class Mk2ArmorHUDOverlay {
-	@SubscribeEvent(priority = EventPriority.NORMAL)
+	@SubscribeEvent(priority = EventPriority.HIGH)
 	public static void eventHandler(RenderGuiEvent.Pre event) {
 		int w = event.getWindow().getGuiScaledWidth();
 		int h = event.getWindow().getGuiScaledHeight();
@@ -53,7 +53,16 @@ public class Mk2ArmorHUDOverlay {
 		RenderSystem.blendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
 		RenderSystem.setShaderColor(1, 1, 1, 1);
 		if (Mk2OverlayDisplayOverlayInGameProcedure.execute(entity)) {
+			event.getGuiGraphics().blit(new ResourceLocation("forged_reactor:textures/screens/ironman_hud_vision.png"), 0, 0, 0, 0, w, h, w, h);
 			event.getGuiGraphics().blit(new ResourceLocation("forged_reactor:textures/screens/ylevel_hud_display.png"), 2, h / 2 + -132, 0, 0, 23, 256, 23, 256);
+
+			event.getGuiGraphics().blit(new ResourceLocation("forged_reactor:textures/screens/top_hud_bar.png"), w / 2 + -518, 3, 0, 0, 1056, 3, 1056, 3);
+
+			event.getGuiGraphics().blit(new ResourceLocation("forged_reactor:textures/screens/repulsors_icon_hud.png"), 32, h - 49, 0, 0, 43, 43, 43, 43);
+
+			event.getGuiGraphics().blit(new ResourceLocation("forged_reactor:textures/screens/missile_icon_hud.png"), w - 60, h - 50, 0, 0, 43, 43, 43, 43);
+
+			event.getGuiGraphics().blit(new ResourceLocation("forged_reactor:textures/screens/test.png"), w / 2 + 111, h / 2 + -112, 0, 0, 100, 100, 100, 100);
 
 			event.getGuiGraphics().drawString(Minecraft.getInstance().font,
 
@@ -64,19 +73,19 @@ public class Mk2ArmorHUDOverlay {
 						YLevelReturnProcedure.execute(entity), 20, h / 2 + -5, -6750208, false);
 			event.getGuiGraphics().drawString(Minecraft.getInstance().font,
 
-					GetEntityNameProcedure.execute(world, entity), w - 91, 2, -1, false);
+					GetEntityNameProcedure.execute(world, entity), w - 96, 12, -1, false);
 			if (GetIFEntityHostileProcedure.execute(world, entity))
 				event.getGuiGraphics().drawString(Minecraft.getInstance().font,
 
-						GetEntityNameProcedure.execute(world, entity), w - 91, 2, -6867656, false);
+						GetEntityNameProcedure.execute(world, entity), w - 96, 12, -6867656, false);
 			event.getGuiGraphics().drawString(Minecraft.getInstance().font,
 
-					GetEntityHealthProcedure.execute(world, entity), w - 91, 15, -1, false);
+					GetEntityHealthProcedure.execute(world, entity), w - 96, 24, -1, false);
 			event.getGuiGraphics().drawString(Minecraft.getInstance().font,
 
-					SpeedReturnProcedure.execute(entity), 20, h / 2 + 7, -1, false);
+					SpeedReturnProcedure.execute(entity), w / 2 + -27, 12, -1, false);
 			if (PlayerHUDRendererProcedure.execute(entity) instanceof LivingEntity livingEntity) {
-				InventoryScreen.renderEntityInInventoryFollowsAngle(event.getGuiGraphics(), w - 32, h - 5, 40, 0f, 0, livingEntity);
+				InventoryScreen.renderEntityInInventoryFollowsAngle(event.getGuiGraphics(), w - 57, h / 2 + 56, 55, 0f, 0, livingEntity);
 			}
 		}
 		RenderSystem.depthMask(true);
