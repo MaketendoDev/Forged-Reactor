396406c775665eb47ff64dcfe4cdb1ba95ab61e6
MaketendoDev <najim.oria59@hotmail.com>
2024-09-29 13:08:31 +0100

    More Flight System Tweaks

--- src/main/java/net/maketendo/forgedreactor/procedures/AccurateFlightSystemProcedure.java
+++ src/main/java/net/maketendo/forgedreactor/procedures/AccurateFlightSystemProcedure.java
@@ -1,11 +1,15 @@
 package net.maketendo.forgedreactor.procedures;
 
+import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.event.TickEvent;
 
 import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.GameType;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.player.Player;
@@ -14,8 +18,10 @@ import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.util.RandomSource;
 import net.minecraft.tags.ItemTags;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.core.BlockPos;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.Minecraft;
 
@@ -26,15 +32,15 @@ public class AccurateFlightSystemProcedure {
 	@SubscribeEvent
 	public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
 		if (event.phase == TickEvent.Phase.END) {
-			execute(event, event.player);
+			execute(event, event.player.level(), event.player.getX(), event.player.getY(), event.player.getZ(), event.player);
 		}
 	}
 
-	public static void execute(Entity entity) {
-		execute(null, entity);
+	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
+		execute(null, world, x, y, z, entity);
 	}
 
-	private static void execute(@Nullable Event event, Entity entity) {
+	private static void execute(@Nullable Event event, LevelAccessor world, double x, double y, double z, Entity entity) {
 		if (entity == null)
 			return;
 		if ((entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.FEET) : ItemStack.EMPTY).is(ItemTags.create(new ResourceLocation("ironman:enableflight")))) {
@@ -69,31 +75,41 @@ public class AccurateFlightSystemProcedure {
 					return false;
 				}
 			}.checkGamemode(entity)) {
-				if (entity instanceof Player _player) {
-					_player.getAbilities().mayfly = true;
-					_player.onUpdateAbilities();
-				}
-				if (entity instanceof Player player && player.getAbilities().flying) {
-					if (Screen.hasControlDown()) {
-						entity.setDeltaMovement(new Vec3((entity.getLookAngle().x * 3), (entity.getLookAngle().y * 3), (entity.getLookAngle().z * 2.5)));
-						{
-							ItemStack _ist = (entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.FEET) : ItemStack.EMPTY);
-							if (_ist.hurt((int) 0.6, RandomSource.create(), null)) {
-								_ist.shrink(1);
-								_ist.setDamageValue(0);
-							}
+				if ((world.getBlockState(BlockPos.containing(x, y, z))).getBlock() == Blocks.WATER) {
+					if (world instanceof Level _level) {
+						if (!_level.isClientSide()) {
+							_level.playSound(null, BlockPos.containing(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("forged_reactor:flighttest_armor_spark")), SoundSource.NEUTRAL, (float) 0.1, (float) 0.1);
+						} else {
+							_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("forged_reactor:flighttest_armor_spark")), SoundSource.NEUTRAL, (float) 0.1, (float) 0.1, false);
 						}
-						entity.fallDistance = 0;
-					} else {
-						entity.setDeltaMovement(new Vec3((entity.getLookAngle().x * 1.4), (entity.getLookAngle().y * 1.4), (entity.getLookAngle().z * 1.4)));
-						{
-							ItemStack _ist = (entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.FEET) : ItemStack.EMPTY);
-							if (_ist.hurt((int) 0.2, RandomSource.create(), null)) {
-								_ist.shrink(1);
-								_ist.setDamageValue(0);
+					}
+				} else {
+					if (entity instanceof Player _player) {
+						_player.getAbilities().mayfly = true;
+						_player.onUpdateAbilities();
+					}
+					if (entity instanceof Player player && player.getAbilities().flying) {
+						if (Screen.hasControlDown()) {
+							entity.setDeltaMovement(new Vec3((entity.getLookAngle().x * 3), (entity.getLookAngle().y * 3), (entity.getLookAngle().z * 2.5)));
+							{
+								ItemStack _ist = (entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.FEET) : ItemStack.EMPTY);
+								if (_ist.hurt((int) 0.6, RandomSource.create(), null)) {
+									_ist.shrink(1);
+									_ist.setDamageValue(0);
+								}
 							}
+							entity.fallDistance = 0;
+						} else {
+							entity.setDeltaMovement(new Vec3((entity.getLookAngle().x * 1.4), (entity.getLookAngle().y * 1.4), (entity.getLookAngle().z * 1.4)));
+							{
+								ItemStack _ist = (entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.FEET) : ItemStack.EMPTY);
+								if (_ist.hurt((int) 0.2, RandomSource.create(), null)) {
+									_ist.shrink(1);
+									_ist.setDamageValue(0);
+								}
+							}
+							entity.fallDistance = 0;
 						}
-						entity.fallDistance = 0;
 					}
 				}
 			}
---
+++ src/main/java/net/maketendo/forgedreactor/procedures/RepulsorProcedure.java
@@ -0,0 +1,9 @@
+package net.maketendo.forgedreactor.procedures;
+
+public class RepulsorProcedure {
+	public static void execute() {
+		double raytrace_distance = 0;
+		String found_entity_name = "";
+		boolean entity_found = false;
+	}
+}
