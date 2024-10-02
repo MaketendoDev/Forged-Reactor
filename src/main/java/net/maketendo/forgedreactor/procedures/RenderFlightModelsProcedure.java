ccbab72364081e84c054c9746317fda9a7028915
MaketendoDev <najim.oria59@hotmail.com>
2024-10-02 21:21:57 +0100

    fixes

--- src/main/java/net/maketendo/forgedreactor/procedures/RenderFlightModelsProcedure.java
+++ src/main/java/net/maketendo/forgedreactor/procedures/RenderFlightModelsProcedure.java
@@ -67,120 +67,122 @@ public class RenderFlightModelsProcedure {
 						return false;
 					}
 				}.checkGamemode(entity))) {
-					RenderLivingEvent _evt = (RenderLivingEvent) event;
-					Minecraft mc = Minecraft.getInstance();
-					EntityRenderDispatcher dis = Minecraft.getInstance().getEntityRenderDispatcher();
-					EntityRendererProvider.Context context = new EntityRendererProvider.Context(dis, mc.getItemRenderer(), mc.getBlockRenderer(), dis.getItemInHandRenderer(), mc.getResourceManager(), mc.getEntityModels(), mc.font);
-					Entity _evtEntity = _evt.getEntity();
-					PlayerRenderer _pr = null;
-					PoseStack poseStack = _evt.getPoseStack();
-					if (_evt.getRenderer() instanceof PlayerRenderer && !(_evt.getRenderer() instanceof com.kleiders.kleidersplayerrenderer.KleidersIgnoreCancel)) {
-						ResourceLocation _texture = new ResourceLocation("kleiders_custom_renderer:textures/entities/empty.png");
-						com.kleiders.kleidersplayerrenderer.KleidersSkinRenderer emptyRenderer = new com.kleiders.kleidersplayerrenderer.KleidersSkinRenderer(context,
-								(_evtEntity instanceof AbstractClientPlayer ? ((AbstractClientPlayer) _evtEntity).getModelName().equals("slim") : false), _texture);
-						_pr = emptyRenderer;
-						emptyRenderer.clearLayers();
-						emptyRenderer.render((AbstractClientPlayer) _evt.getEntity(), _evt.getEntity().getYRot(), _evt.getPartialTick(), _evt.getPoseStack(), _evt.getMultiBufferSource(), _evt.getPackedLight());
-					}
-					if (entity.getPersistentData().getDouble("Model") == 0) {
+					if ((entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.FEET) : ItemStack.EMPTY).getOrCreateTag().getDouble("power") > 10) {
+						RenderLivingEvent _evt = (RenderLivingEvent) event;
+						Minecraft mc = Minecraft.getInstance();
+						EntityRenderDispatcher dis = Minecraft.getInstance().getEntityRenderDispatcher();
+						EntityRendererProvider.Context context = new EntityRendererProvider.Context(dis, mc.getItemRenderer(), mc.getBlockRenderer(), dis.getItemInHandRenderer(), mc.getResourceManager(), mc.getEntityModels(), mc.font);
+						Entity _evtEntity = _evt.getEntity();
+						PlayerRenderer _pr = null;
+						PoseStack poseStack = _evt.getPoseStack();
 						if (_evt.getRenderer() instanceof PlayerRenderer && !(_evt.getRenderer() instanceof com.kleiders.kleidersplayerrenderer.KleidersIgnoreCancel)) {
-							ResourceLocation _texture = new ResourceLocation("kleiders_custom_renderer:textures/entities/default.png");
-							if (ResourceLocation.tryParse("forged_reactor:textures/entities/boots_flame_1.png") != null) {
-								_texture = new ResourceLocation("forged_reactor:textures/entities/boots_flame_1.png");
-							}
-							Modelboots_flame newModel = new Modelboots_flame(context.bakeLayer(Modelboots_flame.LAYER_LOCATION));
-							newModel.LeftLeg.copyFrom(_pr.getModel().leftLeg);
-							newModel.RightLeg.copyFrom(_pr.getModel().rightLeg);
-							newModel.LeftArm.copyFrom(_pr.getModel().leftArm);
-							newModel.RightArm.copyFrom(_pr.getModel().rightArm);
-							newModel.Body.copyFrom(_pr.getModel().body);
-							newModel.Head.copyFrom(_pr.getModel().head);
-							poseStack.pushPose();
-							poseStack.scale(0.9375F, 0.9375F, 0.9375F);
-							new com.kleiders.kleidersplayerrenderer.KleidersPlayerAnimatedRenderer(context, _texture, newModel).render((AbstractClientPlayer) _evt.getEntity(), _evt.getEntity().getYRot(), _evt.getPartialTick(), _evt.getPoseStack(),
-									_evt.getMultiBufferSource(), _evt.getPackedLight());
-							poseStack.popPose();
+							ResourceLocation _texture = new ResourceLocation("kleiders_custom_renderer:textures/entities/empty.png");
+							com.kleiders.kleidersplayerrenderer.KleidersSkinRenderer emptyRenderer = new com.kleiders.kleidersplayerrenderer.KleidersSkinRenderer(context,
+									(_evtEntity instanceof AbstractClientPlayer ? ((AbstractClientPlayer) _evtEntity).getModelName().equals("slim") : false), _texture);
+							_pr = emptyRenderer;
+							emptyRenderer.clearLayers();
+							emptyRenderer.render((AbstractClientPlayer) _evt.getEntity(), _evt.getEntity().getYRot(), _evt.getPartialTick(), _evt.getPoseStack(), _evt.getMultiBufferSource(), _evt.getPackedLight());
 						}
-					} else if (entity.getPersistentData().getDouble("Model") == 1) {
-						if (_evt.getRenderer() instanceof PlayerRenderer && !(_evt.getRenderer() instanceof com.kleiders.kleidersplayerrenderer.KleidersIgnoreCancel)) {
-							ResourceLocation _texture = new ResourceLocation("kleiders_custom_renderer:textures/entities/default.png");
-							if (ResourceLocation.tryParse("forged_reactor:textures/entities/boots_flame_2.png") != null) {
-								_texture = new ResourceLocation("forged_reactor:textures/entities/boots_flame_2.png");
+						if (entity.getPersistentData().getDouble("Model") == 0) {
+							if (_evt.getRenderer() instanceof PlayerRenderer && !(_evt.getRenderer() instanceof com.kleiders.kleidersplayerrenderer.KleidersIgnoreCancel)) {
+								ResourceLocation _texture = new ResourceLocation("kleiders_custom_renderer:textures/entities/default.png");
+								if (ResourceLocation.tryParse("forged_reactor:textures/entities/boots_flame_1.png") != null) {
+									_texture = new ResourceLocation("forged_reactor:textures/entities/boots_flame_1.png");
+								}
+								Modelboots_flame newModel = new Modelboots_flame(context.bakeLayer(Modelboots_flame.LAYER_LOCATION));
+								newModel.LeftLeg.copyFrom(_pr.getModel().leftLeg);
+								newModel.RightLeg.copyFrom(_pr.getModel().rightLeg);
+								newModel.LeftArm.copyFrom(_pr.getModel().leftArm);
+								newModel.RightArm.copyFrom(_pr.getModel().rightArm);
+								newModel.Body.copyFrom(_pr.getModel().body);
+								newModel.Head.copyFrom(_pr.getModel().head);
+								poseStack.pushPose();
+								poseStack.scale(0.9375F, 0.9375F, 0.9375F);
+								new com.kleiders.kleidersplayerrenderer.KleidersPlayerAnimatedRenderer(context, _texture, newModel).render((AbstractClientPlayer) _evt.getEntity(), _evt.getEntity().getYRot(), _evt.getPartialTick(),
+										_evt.getPoseStack(), _evt.getMultiBufferSource(), _evt.getPackedLight());
+								poseStack.popPose();
 							}
-							Modelboots_flame newModel = new Modelboots_flame(context.bakeLayer(Modelboots_flame.LAYER_LOCATION));
-							newModel.LeftLeg.copyFrom(_pr.getModel().leftLeg);
-							newModel.RightLeg.copyFrom(_pr.getModel().rightLeg);
-							newModel.LeftArm.copyFrom(_pr.getModel().leftArm);
-							newModel.RightArm.copyFrom(_pr.getModel().rightArm);
-							newModel.Body.copyFrom(_pr.getModel().body);
-							newModel.Head.copyFrom(_pr.getModel().head);
-							poseStack.pushPose();
-							poseStack.scale(0.9375F, 0.9375F, 0.9375F);
-							new com.kleiders.kleidersplayerrenderer.KleidersPlayerAnimatedRenderer(context, _texture, newModel).render((AbstractClientPlayer) _evt.getEntity(), _evt.getEntity().getYRot(), _evt.getPartialTick(), _evt.getPoseStack(),
-									_evt.getMultiBufferSource(), _evt.getPackedLight());
-							poseStack.popPose();
-						}
-					} else if (entity.getPersistentData().getDouble("Model") == 2) {
-						if (_evt.getRenderer() instanceof PlayerRenderer && !(_evt.getRenderer() instanceof com.kleiders.kleidersplayerrenderer.KleidersIgnoreCancel)) {
-							ResourceLocation _texture = new ResourceLocation("kleiders_custom_renderer:textures/entities/default.png");
-							if (ResourceLocation.tryParse("forged_reactor:textures/entities/boots_flame_3.png") != null) {
-								_texture = new ResourceLocation("forged_reactor:textures/entities/boots_flame_3.png");
+						} else if (entity.getPersistentData().getDouble("Model") == 1) {
+							if (_evt.getRenderer() instanceof PlayerRenderer && !(_evt.getRenderer() instanceof com.kleiders.kleidersplayerrenderer.KleidersIgnoreCancel)) {
+								ResourceLocation _texture = new ResourceLocation("kleiders_custom_renderer:textures/entities/default.png");
+								if (ResourceLocation.tryParse("forged_reactor:textures/entities/boots_flame_2.png") != null) {
+									_texture = new ResourceLocation("forged_reactor:textures/entities/boots_flame_2.png");
+								}
+								Modelboots_flame newModel = new Modelboots_flame(context.bakeLayer(Modelboots_flame.LAYER_LOCATION));
+								newModel.LeftLeg.copyFrom(_pr.getModel().leftLeg);
+								newModel.RightLeg.copyFrom(_pr.getModel().rightLeg);
+								newModel.LeftArm.copyFrom(_pr.getModel().leftArm);
+								newModel.RightArm.copyFrom(_pr.getModel().rightArm);
+								newModel.Body.copyFrom(_pr.getModel().body);
+								newModel.Head.copyFrom(_pr.getModel().head);
+								poseStack.pushPose();
+								poseStack.scale(0.9375F, 0.9375F, 0.9375F);
+								new com.kleiders.kleidersplayerrenderer.KleidersPlayerAnimatedRenderer(context, _texture, newModel).render((AbstractClientPlayer) _evt.getEntity(), _evt.getEntity().getYRot(), _evt.getPartialTick(),
+										_evt.getPoseStack(), _evt.getMultiBufferSource(), _evt.getPackedLight());
+								poseStack.popPose();
 							}
-							Modelboots_flame newModel = new Modelboots_flame(context.bakeLayer(Modelboots_flame.LAYER_LOCATION));
-							newModel.LeftLeg.copyFrom(_pr.getModel().leftLeg);
-							newModel.RightLeg.copyFrom(_pr.getModel().rightLeg);
-							newModel.LeftArm.copyFrom(_pr.getModel().leftArm);
-							newModel.RightArm.copyFrom(_pr.getModel().rightArm);
-							newModel.Body.copyFrom(_pr.getModel().body);
-							newModel.Head.copyFrom(_pr.getModel().head);
-							poseStack.pushPose();
-							poseStack.scale(0.9375F, 0.9375F, 0.9375F);
-							new com.kleiders.kleidersplayerrenderer.KleidersPlayerAnimatedRenderer(context, _texture, newModel).render((AbstractClientPlayer) _evt.getEntity(), _evt.getEntity().getYRot(), _evt.getPartialTick(), _evt.getPoseStack(),
-									_evt.getMultiBufferSource(), _evt.getPackedLight());
-							poseStack.popPose();
-						}
-					} else if (entity.getPersistentData().getDouble("Model") == 3) {
-						if (_evt.getRenderer() instanceof PlayerRenderer && !(_evt.getRenderer() instanceof com.kleiders.kleidersplayerrenderer.KleidersIgnoreCancel)) {
-							ResourceLocation _texture = new ResourceLocation("kleiders_custom_renderer:textures/entities/default.png");
-							if (ResourceLocation.tryParse("forged_reactor:textures/entities/boots_flame_4.png") != null) {
-								_texture = new ResourceLocation("forged_reactor:textures/entities/boots_flame_4.png");
+						} else if (entity.getPersistentData().getDouble("Model") == 2) {
+							if (_evt.getRenderer() instanceof PlayerRenderer && !(_evt.getRenderer() instanceof com.kleiders.kleidersplayerrenderer.KleidersIgnoreCancel)) {
+								ResourceLocation _texture = new ResourceLocation("kleiders_custom_renderer:textures/entities/default.png");
+								if (ResourceLocation.tryParse("forged_reactor:textures/entities/boots_flame_3.png") != null) {
+									_texture = new ResourceLocation("forged_reactor:textures/entities/boots_flame_3.png");
+								}
+								Modelboots_flame newModel = new Modelboots_flame(context.bakeLayer(Modelboots_flame.LAYER_LOCATION));
+								newModel.LeftLeg.copyFrom(_pr.getModel().leftLeg);
+								newModel.RightLeg.copyFrom(_pr.getModel().rightLeg);
+								newModel.LeftArm.copyFrom(_pr.getModel().leftArm);
+								newModel.RightArm.copyFrom(_pr.getModel().rightArm);
+								newModel.Body.copyFrom(_pr.getModel().body);
+								newModel.Head.copyFrom(_pr.getModel().head);
+								poseStack.pushPose();
+								poseStack.scale(0.9375F, 0.9375F, 0.9375F);
+								new com.kleiders.kleidersplayerrenderer.KleidersPlayerAnimatedRenderer(context, _texture, newModel).render((AbstractClientPlayer) _evt.getEntity(), _evt.getEntity().getYRot(), _evt.getPartialTick(),
+										_evt.getPoseStack(), _evt.getMultiBufferSource(), _evt.getPackedLight());
+								poseStack.popPose();
 							}
-							Modelboots_flame newModel = new Modelboots_flame(context.bakeLayer(Modelboots_flame.LAYER_LOCATION));
-							newModel.LeftLeg.copyFrom(_pr.getModel().leftLeg);
-							newModel.RightLeg.copyFrom(_pr.getModel().rightLeg);
-							newModel.LeftArm.copyFrom(_pr.getModel().leftArm);
-							newModel.RightArm.copyFrom(_pr.getModel().rightArm);
-							newModel.Body.copyFrom(_pr.getModel().body);
-							newModel.Head.copyFrom(_pr.getModel().head);
-							poseStack.pushPose();
-							poseStack.scale(0.9375F, 0.9375F, 0.9375F);
-							new com.kleiders.kleidersplayerrenderer.KleidersPlayerAnimatedRenderer(context, _texture, newModel).render((AbstractClientPlayer) _evt.getEntity(), _evt.getEntity().getYRot(), _evt.getPartialTick(), _evt.getPoseStack(),
-									_evt.getMultiBufferSource(), _evt.getPackedLight());
-							poseStack.popPose();
-						}
-					} else {
-						entity.getPersistentData().putDouble("Model", 0);
-						if (_evt.getRenderer() instanceof PlayerRenderer && !(_evt.getRenderer() instanceof com.kleiders.kleidersplayerrenderer.KleidersIgnoreCancel)) {
-							ResourceLocation _texture = new ResourceLocation("kleiders_custom_renderer:textures/entities/default.png");
-							if (ResourceLocation.tryParse("forged_reactor:textures/entities/boots_flame_1.png") != null) {
-								_texture = new ResourceLocation("forged_reactor:textures/entities/boots_flame_1.png");
+						} else if (entity.getPersistentData().getDouble("Model") == 3) {
+							if (_evt.getRenderer() instanceof PlayerRenderer && !(_evt.getRenderer() instanceof com.kleiders.kleidersplayerrenderer.KleidersIgnoreCancel)) {
+								ResourceLocation _texture = new ResourceLocation("kleiders_custom_renderer:textures/entities/default.png");
+								if (ResourceLocation.tryParse("forged_reactor:textures/entities/boots_flame_4.png") != null) {
+									_texture = new ResourceLocation("forged_reactor:textures/entities/boots_flame_4.png");
+								}
+								Modelboots_flame newModel = new Modelboots_flame(context.bakeLayer(Modelboots_flame.LAYER_LOCATION));
+								newModel.LeftLeg.copyFrom(_pr.getModel().leftLeg);
+								newModel.RightLeg.copyFrom(_pr.getModel().rightLeg);
+								newModel.LeftArm.copyFrom(_pr.getModel().leftArm);
+								newModel.RightArm.copyFrom(_pr.getModel().rightArm);
+								newModel.Body.copyFrom(_pr.getModel().body);
+								newModel.Head.copyFrom(_pr.getModel().head);
+								poseStack.pushPose();
+								poseStack.scale(0.9375F, 0.9375F, 0.9375F);
+								new com.kleiders.kleidersplayerrenderer.KleidersPlayerAnimatedRenderer(context, _texture, newModel).render((AbstractClientPlayer) _evt.getEntity(), _evt.getEntity().getYRot(), _evt.getPartialTick(),
+										_evt.getPoseStack(), _evt.getMultiBufferSource(), _evt.getPackedLight());
+								poseStack.popPose();
 							}
-							Modelboots_flame newModel = new Modelboots_flame(context.bakeLayer(Modelboots_flame.LAYER_LOCATION));
-							newModel.LeftLeg.copyFrom(_pr.getModel().leftLeg);
-							newModel.RightLeg.copyFrom(_pr.getModel().rightLeg);
-							newModel.LeftArm.copyFrom(_pr.getModel().leftArm);
-							newModel.RightArm.copyFrom(_pr.getModel().rightArm);
-							newModel.Body.copyFrom(_pr.getModel().body);
-							newModel.Head.copyFrom(_pr.getModel().head);
-							poseStack.pushPose();
-							poseStack.scale(0.9375F, 0.9375F, 0.9375F);
-							new com.kleiders.kleidersplayerrenderer.KleidersPlayerAnimatedRenderer(context, _texture, newModel).render((AbstractClientPlayer) _evt.getEntity(), _evt.getEntity().getYRot(), _evt.getPartialTick(), _evt.getPoseStack(),
-									_evt.getMultiBufferSource(), _evt.getPackedLight());
-							poseStack.popPose();
+						} else {
+							entity.getPersistentData().putDouble("Model", 0);
+							if (_evt.getRenderer() instanceof PlayerRenderer && !(_evt.getRenderer() instanceof com.kleiders.kleidersplayerrenderer.KleidersIgnoreCancel)) {
+								ResourceLocation _texture = new ResourceLocation("kleiders_custom_renderer:textures/entities/default.png");
+								if (ResourceLocation.tryParse("forged_reactor:textures/entities/boots_flame_1.png") != null) {
+									_texture = new ResourceLocation("forged_reactor:textures/entities/boots_flame_1.png");
+								}
+								Modelboots_flame newModel = new Modelboots_flame(context.bakeLayer(Modelboots_flame.LAYER_LOCATION));
+								newModel.LeftLeg.copyFrom(_pr.getModel().leftLeg);
+								newModel.RightLeg.copyFrom(_pr.getModel().rightLeg);
+								newModel.LeftArm.copyFrom(_pr.getModel().leftArm);
+								newModel.RightArm.copyFrom(_pr.getModel().rightArm);
+								newModel.Body.copyFrom(_pr.getModel().body);
+								newModel.Head.copyFrom(_pr.getModel().head);
+								poseStack.pushPose();
+								poseStack.scale(0.9375F, 0.9375F, 0.9375F);
+								new com.kleiders.kleidersplayerrenderer.KleidersPlayerAnimatedRenderer(context, _texture, newModel).render((AbstractClientPlayer) _evt.getEntity(), _evt.getEntity().getYRot(), _evt.getPartialTick(),
+										_evt.getPoseStack(), _evt.getMultiBufferSource(), _evt.getPackedLight());
+								poseStack.popPose();
+							}
 						}
-					}
-					if (!(entity.getDeltaMovement().y() > 1)) {
-						entity.setPose(Pose.SWIMMING);
+						if (!(entity.getDeltaMovement().y() > 1)) {
+							entity.setPose(Pose.SWIMMING);
+						}
 					}
 				}
 			}
