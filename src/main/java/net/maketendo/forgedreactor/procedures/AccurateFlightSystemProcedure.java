21c9df753a79776074bbf383dab27e080cd7ac77
MaketendoDev <najim.oria59@hotmail.com>
2024-10-03 22:26:16 +0100

    version

--- src/main/java/net/maketendo/forgedreactor/procedures/AccurateFlightSystemProcedure.java
+++ src/main/java/net/maketendo/forgedreactor/procedures/AccurateFlightSystemProcedure.java
@@ -6,6 +6,7 @@ import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.event.TickEvent;
 
 import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.Vec2;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.GameType;
@@ -18,8 +19,12 @@ import net.minecraft.world.effect.MobEffects;
 import net.minecraft.util.RandomSource;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.network.chat.Component;
 import net.minecraft.core.BlockPos;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.CommandSource;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.Minecraft;
 
@@ -89,14 +94,26 @@ public class AccurateFlightSystemProcedure {
 									}
 								}.checkGamemode(entity)) {
 									if (entity instanceof Player _player) {
+										_player.getAbilities().flying = true;
+										_player.onUpdateAbilities();
+									}
+								} else {
+									if (entity instanceof Player _player) {
 										_player.getAbilities().flying = false;
 										_player.onUpdateAbilities();
 									}
 								}
 							} else {
-								if (entity instanceof Player _player) {
-									_player.getAbilities().mayfly = true;
-									_player.onUpdateAbilities();
+								if ((entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.FEET) : ItemStack.EMPTY).getOrCreateTag().getDouble("power") == 0) {
+									if (entity instanceof Player _player) {
+										_player.getAbilities().mayfly = false;
+										_player.onUpdateAbilities();
+									}
+								} else {
+									if (entity instanceof Player _player) {
+										_player.getAbilities().mayfly = true;
+										_player.onUpdateAbilities();
+									}
 								}
 								if (entity instanceof Player player && player.getAbilities().flying) {
 									if (Screen.hasControlDown()) {
@@ -111,6 +128,14 @@ public class AccurateFlightSystemProcedure {
 										entity.fallDistance = 0;
 										(entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.FEET) : ItemStack.EMPTY).getOrCreateTag().putDouble("power",
 												((entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.FEET) : ItemStack.EMPTY).getOrCreateTag().getDouble("power") - 10));
+										if (world instanceof ServerLevel _level)
+											_level.getServer().getCommands().performPrefixedCommand(
+													new CommandSourceStack(CommandSource.NULL, new Vec3(x, y, z), Vec2.ZERO, _level, 4, "", Component.literal(""), _level.getServer(), null).withSuppressedOutput(),
+													"particle minecraft:flame ~ ~.1 ~.5 ~ ~-.2 ~ 1 1 force");
+										if (world instanceof ServerLevel _level)
+											_level.getServer().getCommands().performPrefixedCommand(
+													new CommandSourceStack(CommandSource.NULL, new Vec3(x, y, z), Vec2.ZERO, _level, 4, "", Component.literal(""), _level.getServer(), null).withSuppressedOutput(),
+													"particle minecraft:soul_flame ~ ~.1 ~.5 ~ ~-.2 ~ 1 1 force");
 									} else {
 										entity.setDeltaMovement(new Vec3((entity.getLookAngle().x * 1.4), (entity.getLookAngle().y * 1.4), (entity.getLookAngle().z * 1.4)));
 										{
@@ -123,6 +148,10 @@ public class AccurateFlightSystemProcedure {
 										entity.fallDistance = 0;
 										(entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.FEET) : ItemStack.EMPTY).getOrCreateTag().putDouble("power",
 												((entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.FEET) : ItemStack.EMPTY).getOrCreateTag().getDouble("power") - 5));
+										if (world instanceof ServerLevel _level)
+											_level.getServer().getCommands().performPrefixedCommand(
+													new CommandSourceStack(CommandSource.NULL, new Vec3(x, y, z), Vec2.ZERO, _level, 4, "", Component.literal(""), _level.getServer(), null).withSuppressedOutput(),
+													"particle minecraft:flame ~ ~.1 ~.5 ~ ~-.2 ~ 1 1 force");
 									}
 								}
 							}
@@ -176,7 +205,7 @@ public class AccurateFlightSystemProcedure {
 				}
 			}
 		} else {
-			if (!(entity instanceof LivingEntity _livEnt45 && _livEnt45.hasEffect(MobEffects.WEAKNESS))) {
+			if (!(entity instanceof LivingEntity _livEnt52 && _livEnt52.hasEffect(MobEffects.WEAKNESS))) {
 				if ((entity instanceof LivingEntity _entGetArmor ? _entGetArmor.getItemBySlot(EquipmentSlot.FEET) : ItemStack.EMPTY).is(ItemTags.create(new ResourceLocation("ironman:enableflight")))) {
 					if (new Object() {
 						public boolean checkGamemode(Entity _ent) {
