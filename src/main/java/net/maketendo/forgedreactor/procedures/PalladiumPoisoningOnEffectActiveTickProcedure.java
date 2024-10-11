fb290438f1ff425d9426a490f1029d1a93fc97c1
MaketendoDev <najim.oria59@hotmail.com>
2024-10-11 19:33:11 +0100

    lel

--- src/main/java/net/maketendo/forgedreactor/procedures/PalladiumPoisoningOnEffectActiveTickProcedure.java
+++ src/main/java/net/maketendo/forgedreactor/procedures/PalladiumPoisoningOnEffectActiveTickProcedure.java
@@ -12,43 +12,14 @@ public class PalladiumPoisoningOnEffectActiveTickProcedure {
 			return;
 		if (!(entity instanceof LivingEntity _livEnt0 && _livEnt0.hasEffect(ForgedReactorModMobEffects.CHLOROPHYLL_EFFECT.get()))) {
 			if (entity.getPersistentData().getDouble("PalladiumPoisoning") == 0) {
-				entity.getPersistentData().putDouble("PalladiumPoisoning", 200);
+				entity.getPersistentData().putDouble("PalladiumPoisoning", 500);
 			} else {
 				entity.getPersistentData().putDouble("PalladiumPoisoning", (entity.getPersistentData().getDouble("FlightFireModel") - (1 + amplifier)));
 			}
 			if (entity.getPersistentData().getDouble("PalladiumPoisoning") == 0) {
 				entity.getPersistentData().putDouble("Level", (entity.getPersistentData().getDouble("Level") + 1));
-			}
-			if (entity.getPersistentData().getDouble("Level") == 0) {
 				{
-					double _setval = 0;
-					entity.getCapability(ForgedReactorModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
-						capability.Palladium_Poisoning = _setval;
-						capability.syncPlayerVariables(entity);
-					});
-				}
-			}
-			if (entity.getPersistentData().getDouble("Level") == 1) {
-				{
-					double _setval = 1;
-					entity.getCapability(ForgedReactorModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
-						capability.Palladium_Poisoning = _setval;
-						capability.syncPlayerVariables(entity);
-					});
-				}
-			}
-			if (entity.getPersistentData().getDouble("Level") == 2) {
-				{
-					double _setval = 2;
-					entity.getCapability(ForgedReactorModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
-						capability.Palladium_Poisoning = _setval;
-						capability.syncPlayerVariables(entity);
-					});
-				}
-			}
-			if (entity.getPersistentData().getDouble("Level") >= 3) {
-				{
-					double _setval = 3;
+					double _setval = (entity.getCapability(ForgedReactorModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new ForgedReactorModVariables.PlayerVariables())).Palladium_Poisoning + 1;
 					entity.getCapability(ForgedReactorModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
 						capability.Palladium_Poisoning = _setval;
 						capability.syncPlayerVariables(entity);
