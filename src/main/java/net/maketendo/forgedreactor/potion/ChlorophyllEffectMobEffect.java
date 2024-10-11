fce4079f9f85a84f66cb2a11f74e2a410f5a5a21
MaketendoDev <najim.oria59@hotmail.com>
2024-10-11 21:11:49 +0100

    e

--- src/main/java/net/maketendo/forgedreactor/potion/ChlorophyllEffectMobEffect.java
+++ src/main/java/net/maketendo/forgedreactor/potion/ChlorophyllEffectMobEffect.java
@@ -1,13 +1,8 @@
 
 package net.maketendo.forgedreactor.potion;
 
-import net.minecraftforge.client.extensions.common.IClientMobEffectExtensions;
-
-import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.effect.MobEffect;
-import net.minecraft.client.gui.screens.inventory.EffectRenderingInventoryScreen;
-import net.minecraft.client.gui.GuiGraphics;
 
 public class ChlorophyllEffectMobEffect extends MobEffect {
 	public ChlorophyllEffectMobEffect() {
@@ -17,20 +12,5 @@ public class ChlorophyllEffectMobEffect extends MobEffect {
 	@Override
 	public boolean isDurationEffectTick(int duration, int amplifier) {
 		return true;
-	}
-
-	@Override
-	public void initializeClient(java.util.function.Consumer<IClientMobEffectExtensions> consumer) {
-		consumer.accept(new IClientMobEffectExtensions() {
-			@Override
-			public boolean isVisibleInInventory(MobEffectInstance effect) {
-				return false;
-			}
-
-			@Override
-			public boolean renderInventoryText(MobEffectInstance instance, EffectRenderingInventoryScreen<?> screen, GuiGraphics guiGraphics, int x, int y, int blitOffset) {
-				return false;
-			}
-		});
 	}
 }
