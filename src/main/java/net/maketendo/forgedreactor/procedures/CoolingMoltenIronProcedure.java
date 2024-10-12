48b1f0962ea82c888f8f019e20f117ddce44b9b3
MaketendoDev <najim.oria59@hotmail.com>
2024-10-12 19:31:31 +0100

    AAAAAAAAAAAAAAA

---
+++ src/main/java/net/maketendo/forgedreactor/procedures/CoolingMoltenIronProcedure.java
@@ -0,0 +1,39 @@
+package net.maketendo.forgedreactor.procedures;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.entity.item.ItemTossEvent;
+
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.Vec2;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.network.chat.Component;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.CommandSource;
+
+import javax.annotation.Nullable;
+
+@Mod.EventBusSubscriber
+public class CoolingMoltenIronProcedure {
+	@SubscribeEvent
+	public static void onGemDropped(ItemTossEvent event) {
+		execute(event, event.getPlayer().level(), event.getPlayer().getX(), event.getPlayer().getY(), event.getPlayer().getZ(), event.getPlayer());
+	}
+
+	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
+		execute(null, world, x, y, z, entity);
+	}
+
+	private static void execute(@Nullable Event event, LevelAccessor world, double x, double y, double z, Entity entity) {
+		if (entity == null)
+			return;
+		if (world instanceof ServerLevel _level)
+			_level.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, new Vec3(x, y, z), Vec2.ZERO, _level, 4, "", Component.literal(""), _level.getServer(), null).withSuppressedOutput(),
+					"summon item ~ ~ ~ {Item:{id:\"minecraft:grass_block\",Count:1b}}");
+		if (!entity.level().isClientSide())
+			entity.discard();
+	}
+}
